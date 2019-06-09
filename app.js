7a21b48d72337455c5b02cbfabc2005ae8fab533
Jelte Homminga <jeltehomminga@gmail.com>
2019-06-09 10:18:32 +0200

    middleware

---
+++ app.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 4001;
+
+const jellybeanBag = {
+  mystery: {
+    number: 4
+  },
+  lemon: {
+    number: 5
+  },
+  rootBeer: {
+    number: 25
+  },
+  cherry: {
+    number: 3
+  },
+  licorice: {
+    number: 1
+  }
+};
+
+// Logging Middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} Request Received`);
+  next();
+});
+
+// Add your code below:
+app.use("/beans/:beanName", (req, res, next) => {
+  const beanName = req.params.beanName;
+  if (!jellybeanBag[beanName]) {
+    console.log("Response Sent");
+    return res.status(404).send("Bean with that name does not exist");
+  }
+  req.bean = jellybeanBag[beanName];
+  req.beanName = beanName;
+  next();
+});
+
+app.get("/beans/", (req, res, next) => {
+  res.send(jellybeanBag);
+  console.log("Response Sent");
+});
+
+app.post("/beans/", (req, res, next) => {
+  let bodyData = "";
+  req.on("data", data => {
+    bodyData += data;
+  });
+
+  req.on("end", () => {
+    const body = JSON.parse(bodyData);
+    const beanName = body.name;
+    const numberOfBeans = Number(body.number) || 0;
+    jellybeanBag[beanName] = {
+      number: numberOfBeans
+    };
+    res.send(jellybeanBag[beanName]);
+    console.log("Response Sent");
+  });
+});
+
+app.get("/beans/:beanName", (req, res, next) => {
+  res.send(jellybeanBag[req.beanName]);
+  console.log("Response Sent");
+});
+
+app.post("/beans/:beanName/add", (req, res, next) => {
+  let bodyData = "";
+  req.on("data", data => {
+    bodyData += data;
+  });
+
+  req.on("end", () => {
+    const numberOfBeans = Number(JSON.parse(bodyData).number) || 0;
+    jellybeanBag[req.beanName].number += numberOfBeans;
+    res.send(jellybeanBag[req.beanName]);
+    console.log("Response Sent");
+  });
+});
+
+app.post("/beans/:beanName/remove", (req, res, next) => {
+  let bodyData = "";
+  req.on("data", data => {
+    bodyData += data;
+  });
+
+  req.on("end", () => {
+    const numberOfBeans = Number(JSON.parse(bodyData).number) || 0;
+    jellybeanBag[req.beanName].number -= numberOfBeans;
+    res.send(jellybeanBag[req.beanName]);
+    console.log("Response Sent");
+  });
+});
+
+app.delete("/beans/:beanName", (req, res, next) => {
+  jellybeanBag[req.beanName] = null;
+  res.status(204).send();
+  console.log("Response Sent");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
